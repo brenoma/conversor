354fc96ed865a617e2ef06d1cdda0d5a092fe309
Breno Menezes <brenoma@live.com>
2020-03-03 15:06:55 -0300

    Implementacao

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+// import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const lower = "lower";
+const upper = "upper";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {message:""};
+  }
+
+  converter(action){
+
+    let message = this.state.message;
+
+    switch(action){
+      case lower:
+        console.log("lower");
+        
+        message = message.toLowerCase();
+        break;
+
+      case upper:
+        console.log("upper");
+
+        message = message.toUpperCase();
+        break;
+
+      default:
+        break;
+
+    }
+
+    this.setState({message});
+
+  }
+
+  render() {
+    return (
+      <div className="App">    
+      
+      <textarea onChange={(evt)=>{
+        let message = evt.target.value;
+        this.setState({message});
+      }}
+      className="textarea"
+      value={this.state.message}
+      />
+
+        <div>
+          <button onClick={()=>{this.converter(lower)}}>lowercase</button>
+          <button onClick={()=> {this.converter(upper)}}>UPPERCASE</button>  
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
